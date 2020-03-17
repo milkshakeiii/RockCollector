3ef8418c6acea8939bb3248bac2b18435c3c9414
milkshakeiii <milkshakeiii@gmail.com>
2020-03-17 09:29:42 -0700

    one-message accounts

--- rockcollectordjango/webapi/views.py
+++ rockcollectordjango/webapi/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 def index(request):
     username = request.GET['username']
     password = request.GET['password']
     user = authenticate(request, username=username, password=password)
+    if not User.objects.filter(username=username).exists():
+        user = User.objects.create_user(username, 'noemail@noemail.com', password)
     if user is not None:
         login(request, user)
         return HttpResponse("Success.")
