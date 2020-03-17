50de94181b47b7d44ea03213fcd939932d3ca6bc
Henry Solberg <milkshakeiii@gmail.com>
2020-03-16 21:14:34 -0700

    silly auth

--- rockcollectordjango/webapi/views.py
+++ rockcollectordjango/webapi/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
-
+from django.contrib.auth import authenticate, login
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    username = request.GET['username']
+    password = request.GET['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponse("Success.")
+    else:
+        return HttpResponse("Invalid login, yo.")
