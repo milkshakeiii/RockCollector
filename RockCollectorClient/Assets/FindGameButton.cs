b29e9b65001c6386a3e99e69e8c826f3840044c0
milkshakeiii <milkshakeiii@gmail.com>
2020-04-12 22:15:35 -0700

    unity http requests

---
+++ RockCollectorClient/Assets/FindGameButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindGameButton : MonoBehaviour
+{
+    public TMPro.TMP_InputField usernameInput;
+    public TMPro.TMP_InputField wordpassInput;
+
+    public void FindGame()
+    {
+        HttpCommunicator.GetInstance().FindGameRequest(usernameInput.text, wordpassInput.text);
+    }
+}
--- RockCollectorClient/Assets/HttpCommunicator.cs
+++ RockCollectorClient/Assets/HttpCommunicator.cs
@@ -1,18 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class HttpCommunicator : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private string username = "";
+    private string wordpass = "";
+
+    private static HttpCommunicator instance;
+
     void Start()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public static HttpCommunicator GetInstance()
     {
-        
+        return instance;
+    }
+
+    public void FindGameRequest(string newUsername, string newWordpass)
+    {
+        username = newUsername;
+        wordpass = newWordpass;
+
+        StartCoroutine(DoRequest(new Dictionary<string, string>(), "webapi/find_game"));
+    }
+
+    IEnumerator DoRequest(Dictionary<string, string> formData, string endpoint)
+    {
+        if (username.Equals("") || wordpass.Equals(""))
+            throw new UnityException("uername or wordpass not set");
+
+        formData["username"] = username;
+        formData["wordpass"] = wordpass;
+
+        UnityWebRequest www = UnityWebRequest.Post("http://127.0.0.1:8000/"+endpoint, formData);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("http response:");
+            Debug.Log(www.downloadHandler.text);
+        }
     }
 }
