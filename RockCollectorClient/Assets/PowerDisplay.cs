0b3ac539e8f1c536af4704612d67c64a54c78f21
Henry Solberg <milkshakeiii@gmail.com>
2024-08-28 20:01:07 -0700

    power percent text

--- RockCollectorClient/Assets/PowerDisplay.cs
+++ RockCollectorClient/Assets/PowerDisplay.cs
@@ -6,10 +6,13 @@ public class PowerDisplay : MonoBehaviour
 {
     public Submarine submarine;
     public UpAndDownIndicatorArrow powerArrow;
+    public TMPro.TMP_Text powerText;
 
     // Update is called once per frame
     void Update()
     {
         powerArrow.SetHeight(submarine.PowerRemaining() / submarine.maxPower);
+        // display power percentage remaining in the text
+        powerText.text = (submarine.PowerRemaining() / submarine.maxPower * 100).ToString("F0") + "%";
     }
 }
