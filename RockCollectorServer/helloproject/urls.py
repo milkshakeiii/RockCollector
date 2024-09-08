c552ef765e3fa13a180f3bfdcc62289e0b9fd078
Henry <milkshakeiii@gmail.com>
2024-09-07 20:20:40 -0700

    add server skeleton

---
+++ RockCollectorServer/helloproject/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+from rest_framework.authtoken import views
+
+urlpatterns = [
+    path('api-token-auth/', views.obtain_auth_token),
+    path('', include('helloapp.urls')),
+]
