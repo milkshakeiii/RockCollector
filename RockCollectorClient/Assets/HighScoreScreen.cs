dbeb38dd6034eecfc20827cfbbe125b18f13e8ba
Henry Solberg <milkshakeiii@gmail.com>
2024-09-12 20:08:18 -0700

    more mocking

--- RockCollectorClient/Assets/HighScoreScreen.cs
+++ RockCollectorClient/Assets/HighScoreScreen.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class HighScoreScreen : MonoBehaviour
 {
+    public TMPro.TMP_Dropdown cohortDropdown;
+    public TMPro.TMP_Dropdown scoreTypeDropdown;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
