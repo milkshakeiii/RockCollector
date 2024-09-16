3dca8186a55c7da3c096a2c62160479be7c19804
Henry Solberg <milkshakeiii@gmail.com>
2024-09-16 14:06:55 -0700

    further improve environment generation

--- RockCollectorClient/Assets/Environment.cs
+++ RockCollectorClient/Assets/Environment.cs
@@ -31,17 +31,34 @@ public class Environment : MonoBehaviour
 
         // start with a grid to make into the terrain shape
         bool[,] terrainGrid = new bool[width, height];
+        
+        int mountainStartX = random.Next(30, 100);
+        int mountainStartY = 0;
+        int mountainEndX = Mathf.Min(mountainStartX + random.Next(30, 100), width-1);
+        int mountainEndY = Mathf.Min(mountainStartY + random.Next(30, 100), height-1);
+        int mountainWidth = mountainEndX - mountainStartX;
+        int mountainCenter = random.Next(mountainStartX + mountainWidth / 4, mountainEndX - mountainWidth / 4);
+        Debug.Log("Mountain start: " + mountainStartX + ", " + mountainStartY);
+        Debug.Log("Mountain end: " + mountainEndX + ", " + mountainEndY);
+        Debug.Log("Mountain center: " + mountainCenter);
+        IEnumerator mountainEnumerator = AddMountain(random, mountainStartX, mountainStartY, mountainEndX, mountainEndY, mountainCenter, terrainGrid);
+        while (mountainEnumerator.MoveNext())
+        {
+            yield return null;
+        }
+
         // each grid cell covers 4 sprites with 4 rotations
         Sprite[,] sprites = new Sprite[width * 2, height * 2];
         int[,] rotations = new int[width * 2, height * 2];
+        IEnumerator spritesEnumerator = DecideSpritesAndRotations(random, terrainGrid, sprites, rotations);
+        while (spritesEnumerator.MoveNext())
+        {
+            yield return null;
+        }
 
-        int mountainStartX = random.Next(30, 100);
-        int mountainStartY = 0;
-        int mountainEndX = mountainStartX + random.Next(30, 100);
-        int mountainEndY = mountainStartY + random.Next(30, 100);
-        int mountainCenter = random.Next(mountainEndX / 4, 3 * mountainEndX / 4);
-        IEnumerator mountainEnumerator = AddMountain(random, mountainStartX, mountainStartY, mountainEndX, mountainEndY, mountainCenter, terrainGrid, sprites, rotations);
-        while (mountainEnumerator.MoveNext())
+        // spawn the GameObjects
+        IEnumerator spawnEnumerator = SpawnGameObjects(sprites, rotations);
+        while (spawnEnumerator.MoveNext())
         {
             yield return null;
         }
@@ -49,7 +66,7 @@ public class Environment : MonoBehaviour
         lastSeed = seed;
     }
 
-    private IEnumerator AddMountain(System.Random random, int startX, int startY, int endX, int endY, int center, bool[,] grid, Sprite[,] sprites, int[,] rotations)
+    private IEnumerator AddMountain(System.Random random, int startX, int startY, int endX, int endY, int center, bool[,] grid)
     {
 
         int currentX = startX;
@@ -60,7 +77,7 @@ public class Environment : MonoBehaviour
         {
             if (currentY < endY && currentY >= 0)
             {
-                Debug.Log("xIndex: " + currentX + " yIndex: " + currentY);
+                //Debug.Log("xIndex: " + currentX + " yIndex: " + currentY);
                 grid[currentX, currentY] = true;
             }
             currentX++;
@@ -90,21 +107,26 @@ public class Environment : MonoBehaviour
             }
             yield return null;
         }
+    }
 
+    private IEnumerator DecideSpritesAndRotations(System.Random random, bool[,] grid, Sprite[,] sprites, int[,] rotations)
+    {
+        int width = grid.GetLength(0);
+        int height = grid.GetLength(1);
         // set filler, edge, inner corner, and inner edge rocks
-        for (int x = 0; x < endX; x++)
+        for (int x = 0; x < width; x++)
         {
-            for (int y = 0; y < endY; y++)
+            for (int y = 0; y < height; y++)
             {
                 if (!grid[x, y])
                 {
                     continue;
                 }
-                bool upLeftOccupied = x > 0 && y < endY - 1 && grid[x - 1, y + 1];
-                bool upOccupied = y < endY - 1 && grid[x, y + 1];
-                bool upRightOccupied = x < endX - 1 && y < endY - 1 && grid[x + 1, y + 1];
-                bool rightOccupied = x < endX - 1 && grid[x + 1, y];
-                bool downRightOccupied = x < endX - 1 && y > 0 && grid[x + 1, y - 1];
+                bool upLeftOccupied = x > 0 && y < height - 1 && grid[x - 1, y + 1];
+                bool upOccupied = y < height - 1 && grid[x, y + 1];
+                bool upRightOccupied = x < width - 1 && y < height - 1 && grid[x + 1, y + 1];
+                bool rightOccupied = x < width - 1 && grid[x + 1, y];
+                bool downRightOccupied = x < width - 1 && y > 0 && grid[x + 1, y - 1];
                 bool downOccupied = y > 0 && grid[x, y - 1];
                 bool downLeftOccupied = x > 0 && y > 0 && grid[x - 1, y - 1];
                 bool leftOccupied = x > 0 && grid[x - 1, y];
@@ -226,16 +248,21 @@ public class Environment : MonoBehaviour
             }
             yield return null;
         }
+    }
 
+    private IEnumerator SpawnGameObjects(Sprite[,] sprites, int[,] rotations)
+    {
+        int width = sprites.GetLength(0);
+        int height = sprites.GetLength(1);
         // spawn GameObjects with sprites
-        for (int x = 0; x < endX * 2; x++)
+        for (int x = 0; x < width; x++)
         {
-            for (int y = 0; y < endY * 2; y++)
+            for (int y = 0; y < height; y++)
             {
                 if (sprites[x, y] != null)
                 {
                     GameObject rock = new("Rock");
-                    rock.transform.position = new Vector3((-endX / 2 + x) * rockSpacing + 5, (-endY + y) * rockSpacing - 10, 0);
+                    rock.transform.position = new Vector3((-width / 2 + x) * rockSpacing + 5, (-height + y) * rockSpacing - 10, 0);
                     rock.AddComponent<SpriteRenderer>().sprite = sprites[x, y];
                     // also add a collider if this is not a filler rock
                     if (sprites[x, y] != fillerRock)
@@ -246,69 +273,7 @@ public class Environment : MonoBehaviour
                     rock.transform.Rotate(Vector3.forward, rotations[x, y]);
                 }
             }
-        }
-
-        // place some inner props randomly
-        for (int i = 0; i < 200; i++)
-        {
-            // randomly choose a prop list
-            List<Sprite> propList = random.Next(0, 2) == 0 ? inner_prop_1 : inner_prop_2;
-            // randomly choose up, down, left, or right
-            List<Vector2Int> directions = new() { new Vector2Int(0, 1), new Vector2Int(0, -1), new Vector2Int(-1, 0), new Vector2Int(1, 0) };
-            Vector2Int direction = directions[random.Next(0, 4)];
-
-            // randomly choose a position
-            int x = random.Next(0, endX * 2);
-            int y = random.Next(0, endY * 2);
-
-            // add direction to get the positions of all the sprites
-            List<Vector2Int> positions = new() { new Vector2Int(x, y) };
-            for (int j = 0; j < propList.Count - 1; j++)
-            {
-                positions.Add(positions[j] + direction);
-            }
-
-            // only continue if all positions are within bounds and are currently filler rock
-            bool valid = true;
-            foreach (Vector2Int position in positions)
-            {
-                if (position.x < 0 || position.x >= endX * 2 || position.y < 0 || position.y >= endY * 2 || sprites[position.x, position.y] != fillerRock)
-                {
-                    valid = false;
-                    break;
-                }
-            }
-            if (!valid)
-            {
-                continue;
-            }
-
-            // spawn the game objects
-            for (int j = 0; j < propList.Count; j++)
-            {
-                GameObject prop = new("Prop");
-                prop.transform.position = new Vector3((-endX / 2 + positions[j].x) * rockSpacing + 5, (-endY + positions[j].y) * rockSpacing - 10, 0);
-                prop.AddComponent<SpriteRenderer>().sprite = propList[j];
-                prop.GetComponent<SpriteRenderer>().sortingOrder = 1;
-                prop.layer = 9;
-                // set the rotation according to the direction
-                if (direction == new Vector2Int(0, -1))
-                {
-                    prop.transform.Rotate(Vector3.forward, 270f);
-                }
-                else if (direction == new Vector2Int(1, 0))
-                {
-                    prop.transform.Rotate(Vector3.forward, 0f);
-                }
-                else if (direction == new Vector2Int(0, 1))
-                {
-                    prop.transform.Rotate(Vector3.forward, 90f);
-                }
-                else if (direction == new Vector2Int(-1, 0))
-                {
-                    prop.transform.Rotate(Vector3.forward, 180f);
-                }
-            }
+            yield return null;
         }
     }
 }
