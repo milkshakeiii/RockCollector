60e4bbb33f4f4ca6aeb8e1414715dd9976dd0e2a
Henry Solberg <milkshakeiii@gmail.com>
2024-11-09 15:41:42 -0800

    move scripts

---
+++ RockCollectorClient/Assets/Scripts/SmoothScripts/BattleScreen.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class BattleScreen : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void InitializeBattle(List<Timefish> playerFishStorage, List<Timefish> enemies)
+    {
+
+    }
+}
+
+public class Attack
+{
+
+}
