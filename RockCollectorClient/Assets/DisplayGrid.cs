6e2968a40e1fe75d5755ea72ec30e3e0de3bd035
Henry Solberg <milkshakeiii@gmail.com>
2024-11-10 15:08:52 -0800

    move art assets

--- RockCollectorClient/Assets/DisplayGrid.cs
+++ RockCollectorClient/Assets/DisplayGrid.cs
@@ -2,15 +2,49 @@ using UnityEngine;
 
 public class DisplayGrid : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public const int WIDTH = 240;
+    public const int HEIGHT = 135;
+
+    public GameObject baseSquarePrefab;
+
+
+    void OnEnable()
+    {
+        DisplaySprite("Art/UI/plain_white", 0, 0, WIDTH/3, HEIGHT);
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DisplaySprite(string spriteName, uint x, uint y, uint width, uint height)
     {
-        
+        // create a new GameObject
+        GameObject newSquare = Instantiate(baseSquarePrefab, this.transform.position, Quaternion.identity, this.transform);
+        newSquare.transform.localPosition = new Vector3(x, y, 0);
+        Sprite sprite = Resources.Load<Sprite>(spriteName);
+
+        uint spritePixelWidth = (uint)sprite.rect.width;
+        uint spritePixelHeight = (uint)sprite.rect.height;
+
+        // pixel width and height must be divisible by 8
+        if (spritePixelWidth % 8 != 0 || spritePixelHeight % 8 != 0)
+        {
+            Debug.LogError("Sprite width and height must be divisible by 8");
+            return;
+        }
+
+        uint spriteFullWidth = spritePixelWidth / 8;
+        uint spriteFullHeight = spritePixelHeight / 8;
+
+        uint scaleX = width / spriteFullWidth;
+        uint scaleY = height / spriteFullHeight;
+
+        // set the sprite
+        newSquare.GetComponent<SpriteRenderer>().sprite = sprite;
+
+        // set the scale
+        newSquare.transform.localScale = new Vector3(scaleX, scaleY, 1);
     }
 }
