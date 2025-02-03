c285922a44840910ede94fd59dbc930b1b130fad
Henry Solberg <henry.j.solberg@gmail.com>
2025-02-02 23:17:43 -0800

    clean up file structure

--- RockCollectorServer/playapp/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-import playapp.models as models
-
-
-class ScoreSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Score
-        fields = '__all__'
